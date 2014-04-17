610c5a00b371586b61eb044eb61d75151bb78ed3
Michael Tolan <michaeltolan@Michaels-MacBook-Pro.local>
2014-04-16 19:26:14 -0500

    Added draft JSON file and started messing with functions

---
+++ Project3/sdi-project3/storeInventoryJSON.js
@@ -0,0 +1,59 @@
+var storeInventoryJSON = {
+	"walmart": {
+		"groceries": [
+			"eggs",
+			"milk",
+			"fruit",
+			"vegetables",
+			"meat"
+		],
+		"tech": [
+			"tv",
+			"computer",
+			"game",
+			"phone",
+			"movie"
+		],
+		"auto": [
+			"tire",
+			"wiper",
+			"filter",
+			"tools",
+			"oil"
+		]
+	},
+	"target": {
+		"apparel": [
+			"shirt",
+			"pants",
+			"shoes"
+		],
+		"toys": [
+			"puzzle",
+			"doll",
+			"bike"
+		],
+		"baby": [
+			"diapers",
+			"bottle",
+			"formula"
+		]
+	},
+	"sams": {
+		"outdoor": [
+			"tent",
+			"raft",
+			"grill"
+		],
+		"home": [
+			"bed",
+			"table",
+			"desk"
+		],
+		"hygene": [
+			"shampoo",
+			"toothpaste",
+			"deoderant"
+		]
+	}
+};
--- Project3/sdi-project3/tolan_michael_assignment3.js
+++ Project3/sdi-project3/tolan_michael_assignment3.js
@@ -3,3 +3,29 @@
  * Project 3
  * Shopping List
  */
+
+//GLOBAL VARIABLES
+var shoppingList = [],
+	listLength = 0;
+
+//FUNCTIONS
+var showJSON = function(json) {
+	console.log("show function works.");
+	console.log(json);
+};
+
+var addItem = function(item, list) {
+	listLength = list.push(item);
+	console.log("The item '" + item + "' was added to the list '" + list + "'.");
+};
+
+var removeItem = function(item, list) {
+	list.splice(list.indexOf(item,1));
+};
+
+var listLength = function (list) {
+	return list.length();
+};
+
+//MAIN BODY
+showJSON(storeInventoryJSON);
