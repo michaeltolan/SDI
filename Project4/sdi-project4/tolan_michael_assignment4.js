caaecf337eaa41ffd999fd156f2445dec0a5c316
Michael Tolan <michaeltolan@Michaels-MacBook-Pro.local>
2014-04-24 14:26:43 -0500

    Added all project files and empty funtions.

---
+++ Project4/sdi-project4/tolan_michael_assignment4.js
@@ -0,0 +1,64 @@
+/* Michael Tolan
+ * Apr 24, 2014
+ * Project 4
+ * Problem Solving Functions
+ */
+
+//-------------------------------------------------------------------------------//
+//STRING FUNCTIONS
+
+var isPhoneNumber = function(phoneNumber) {
+	return false;
+};
+
+var isEmailAddress = function(emailAddress) {
+	return false;
+};
+
+var isURL = function(url) {
+	return false;
+};
+
+var titleCase = function(str) {
+	return str;
+};
+
+var replaceSeparator = function(str, sep1, sep2) {
+	return str;
+};
+
+//-------------------------------------------------------------------------------//
+//NUMBER FUNCTIONS
+
+var truncateDecimal = function(number, decimal) {
+	return number;
+};
+
+var fuzzyMatch = function(num1, num2, percent) {
+	return ?;
+};
+
+var dateDiff = function(date1, date2, hORd) {
+	return ?;
+};
+
+var toNumber = function(str) {
+	return num;
+};
+
+//-------------------------------------------------------------------------------//
+//ARRAY FUNCTIONS
+
+var nextHighest = function(array, num) {
+	return num;
+};
+
+var arrayNumSum = function(array) {
+	return total;
+};
+
+var sortArray = function(array, key) {
+	return array;
+};
+
+
