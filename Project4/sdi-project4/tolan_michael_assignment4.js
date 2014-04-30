e9b8ca9a03ae24b460cbf1b1ee8910b28b5ed6b5
Michael Tolan <michaeltolan@Michaels-MacBook-Pro.local>
2014-04-30 17:11:44 -0500

    Finished Phone Number Function

--- Project4/sdi-project4/tolan_michael_assignment4.js
+++ Project4/sdi-project4/tolan_michael_assignment4.js
@@ -8,7 +8,28 @@
 //STRING FUNCTIONS
 
 var isPhoneNumber = function(phoneNumber) {
-	return false;
+	var firstPart,
+		secondPart,
+		thirdPart,
+		remainder;
+		
+	if(phoneNumber.indexOf("-",0) !== -1)
+	{
+		firstPart = phoneNumber.substr(0,3);
+		secondPart = phoneNumber.substr(4,3);
+		thirdPart = phoneNumber.substr(8,4);
+		remainder = phoneNumber.substr(12, (phoneNumber.length - 10));
+	}
+	else {
+		firstPart = phoneNumber.substr(0,3);
+		secondPart = phoneNumber.substr(3,3);
+		thirdPart = phoneNumber.substr(6,4);
+		remainder = phoneNumber.substr(10, (phoneNumber.length - 10));
+	}
+	
+	if(isNaN(firstPart) === true || isNaN(secondPart) === true || isNaN(thirdPart) === true) return false; 
+	else if (remainder.length > 0) return false; 
+	else return true;
 };
 
 var isEmailAddress = function(emailAddress) {
